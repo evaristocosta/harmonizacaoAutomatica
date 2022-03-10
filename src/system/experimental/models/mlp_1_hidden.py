bdc91b97bcd9f12d906eb8961ef896377c443d39
Lucas Costa <lucascosta74@gmail.com>
2022-03-10 01:52:27 -0300

    nova reorganizacao e sistemas

---
+++ src/system/experimental/models/mlp_1_hidden.py
@@ -0,0 +1,18 @@
+import numpy as np
+from tensorflow.keras.optimizers import SGD, Adam
+import keras
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import (
+    Input,
+    Dense,
+)
+
+
+def mlp_1_hidden(params):
+    model = Sequential()
+    model.add(Input(shape=(params["input_shape"],)))
+    model.add(Dense(params["neurons"], activation=params["activation"]))
+    model.add(Dense(params["output_shape"], activation="softmax"))
+
+    return model
