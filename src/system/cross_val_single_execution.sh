767b1a93afe6dc2bcbe4520a84522feab38e68f9
Lucas Costa <lucascosta74@gmail.com>
2022-04-05 10:35:06 -0300

    argumento de modelo

--- src/system/cross_val_single_execution.sh
+++ src/system/cross_val_single_execution.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+MODEL=$1
+NEURONS=$2
 TIMESTAMP=$(date +%s)
 ROLLS=30
 
 for i in $(seq $ROLLS); do
-    python src/system/cross_val_single.py --time $TIMESTAMP --roll $i
+    python src/system/cross_val_single.py --time $TIMESTAMP --roll $i --model $MODEL --neurons $NEURONS
 done
 
-python src/system/cross_val_single_evaluate.py --time $TIMESTAMP
+python src/system/cross_val_single_evaluate.py --time $TIMESTAMP --model $MODEL --neurons $NEURONS
