50e42952855fad8939843baf44489e89fbda7e14
Lucas Costa <lucascosta74@gmail.com>
2022-04-05 11:30:54 -0300

    nohup adicionado

--- src/system/cross_val_single_execution.sh
+++ src/system/cross_val_single_execution.sh
@@ -6,7 +6,7 @@ TIMESTAMP=$(date +%s)
 ROLLS=30
 
 for i in $(seq $ROLLS); do
-    python src/system/cross_val_single.py --time $TIMESTAMP --roll $i --model $MODEL --neurons $NEURONS
+    nohup python src/system/cross_val_single.py --time $TIMESTAMP --roll $i --model $MODEL --neurons $NEURONS &> ${MODEL}_${NEURONS}.out
 done
 
 python src/system/cross_val_single_evaluate.py --time $TIMESTAMP --model $MODEL --neurons $NEURONS
