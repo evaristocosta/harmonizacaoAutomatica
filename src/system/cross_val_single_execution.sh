de37b124a9575d3394f609ebb93c906398598a8e
Lucas Costa <lucascosta74@gmail.com>
2022-04-07 17:13:25 -0300

    erro no comando

--- src/system/cross_val_single_execution.sh
+++ src/system/cross_val_single_execution.sh
@@ -9,4 +9,4 @@ for i in $(seq $ROLLS); do
     nohup python src/system/cross_val_single.py --time $TIMESTAMP --roll $i --model $MODEL --neurons $NEURONS &> ${MODEL}_${NEURONS}.out
 done
 
-python src/system/cross_val_single_evaluate.py --time $TIMESTAMP --model $MODEL --neurons $NEURONS
+python src/system/cross_val_single_evaluate.py --time $TIMESTAMP --model $MODEL
