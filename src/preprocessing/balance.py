f26a7e46416210b84369389e25230963689aa9c2
Lucas Costa <lucascosta74@gmail.com>
2022-04-30 14:31:48 -0300

    underbalance desenhado

---
+++ src/preprocessing/balance.py
@@ -0,0 +1,32 @@
+import numpy as np
+from imblearn.under_sampling import (
+    RandomUnderSampler,
+    NearMiss,
+    EditedNearestNeighbours,
+    RepeatedEditedNearestNeighbours,
+    AllKNN,
+    CondensedNearestNeighbour,
+    OneSidedSelection,
+    NeighbourhoodCleaningRule,
+    InstanceHardnessThreshold,
+)
+from collections import Counter
+
+data = "encoded"
+
+X = np.load("data/" + data + "/vetor_entrada.npy", mmap_mode="r")
+Y = np.load("data/" + data + "/vetor_saida.npy", mmap_mode="r")
+
+original_input_shape = X.shape[1:]
+
+X_reshaped = np.reshape(X, (X.shape[0], -1))
+
+ros = RandomUnderSampler(random_state=0)
+X_resampled, Y_resampled = ros.fit_resample(X_reshaped, Y)
+
+X_resampled_reshaped = np.reshape(
+    X_resampled, ((X_resampled.shape[0],) + original_input_shape)
+)
+
+Y_cat = np.argmax(Y_resampled, axis=1)
+print(sorted(Counter(Y_cat).items()))
