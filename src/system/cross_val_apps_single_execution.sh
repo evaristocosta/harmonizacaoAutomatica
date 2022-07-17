b1d6afcaf4754cc93aa6c44dde8fae808eed278b
Lucas Costa <lucascosta74@gmail.com>
2022-07-16 21:45:27 -0300

    correcao de soma

--- src/system/cross_val_apps_single_execution.sh
+++ src/system/cross_val_apps_single_execution.sh
@@ -9,16 +9,17 @@ MOMENTUM=$6
 NESTEROV=$7
 TIMESTAMP=$(date +%s)
 ROLLS=$8
+ROLLSTART=$9
 
 for i in $(seq $ROLLS); do
-    nohup python src/system/cross_val_single.py --time $TIMESTAMP --roll $i --model $MODEL --batch $BATCH --epoch $EPOCH --optimizer $OPTIMIZER --lr $LR --momentum $MOMENTUM --nesterov $NESTEROV &> ${MODEL}_${BATCH}.out
+    nohup python src/system/cross_val_single.py --time $TIMESTAMP --roll $((i+ROLLSTART)) --model $MODEL --batch $BATCH --epoch $EPOCH --optimizer $OPTIMIZER --lr $LR --momentum $MOMENTUM --nesterov $NESTEROV &> ${MODEL}_${BATCH}.out
 done
 
 python src/system/cross_val_single_evaluate.py --time $TIMESTAMP --model $MODEL
 
 
 # src/system/cross_val_apps_single_execution.sh alexnet 128 90 sgd 0.1 0.9 0 &
-# src/system/cross_val_apps_single_execution.sh vgg16 256 100 sgd 0.1 0.9 0 &
-# src/system/cross_val_apps_single_execution.sh resnet101 256 400 sgd 0.0001 0.9 0 &
-# src/system/cross_val_apps_single_execution.sh inceptionv3 32 100 rmsprop 0.045 1.0 0 &
-# src/system/cross_val_apps_single_execution.sh densenet201 256 90 sgd 0.1 0.9 1 &
+# src/system/cross_val_apps_single_execution.sh vgg16 256 100 sgd 0.1 0.9 0 10 0 &
+# src/system/cross_val_apps_single_execution.sh resnet101 256 400 sgd 0.0001 0.9 0 5 5 &
+# src/system/cross_val_apps_single_execution.sh inceptionv3 32 100 rmsprop 0.045 1.0 0 10 0 &
+# src/system/cross_val_apps_single_execution.sh densenet201 256 90 sgd 0.1 0.9 1 10 0 &
