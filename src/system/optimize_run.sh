dc4ce24988e6c7706156a26d2afaffc732225e10
Lucas Costa <lucascosta74@gmail.com>
2022-08-15 22:28:17 -0300

    correcao do script

--- src/system/optimize_run.sh
+++ src/system/optimize_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for i in $(seq 50); do
-    nohup python src/system/optimize_keras_tuner.py &> alexnet_opt_${i}.out
+    nohup python src/system/optimize_keras_tuner.py &> "alexnet_opt_$i".out
 done
 
 sudo poweroff
